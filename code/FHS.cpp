98116112b4943c9a8ab6b58d2c506de916a0dcea
Frederic Desgreniers <fred@frde.me>
2017-07-04 21:12:50 -0400

    Moved includes to own file, makes FHS.cpp cleaner

--- code/FHS.cpp
+++ code/FHS.cpp
@@ -1,19 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <thread>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <string>
-#include <cstring>
-#include <sstream>
-#include <vector>
-#include <map>
-#include <regex>
-
-#include "Socket.cpp"
-#include "HttpServer.cpp"
-#include "FileLoader.cpp"
-#include "Template.cpp"
+#include "includes.h"
 
 void handleIndex(HttpRequest* request, HttpResponse* response)
 {
---
+++ code/includes.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <regex>
+
+#include "Socket.cpp"
+#include "HttpServer.cpp"
+#include "FileLoader.cpp"
+#include "Template.cpp"
+
+
