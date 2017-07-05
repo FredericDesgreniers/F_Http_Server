7d59b8d483b0dadced4a271b1579dab8b3555fb9
Frederic Desgreniers <fred@frde.me>
2017-07-04 21:10:57 -0400

    Comments

--- code/Template.cpp
+++ code/Template.cpp
@@ -7,6 +7,8 @@ Template::Template(std::string name) : name(name)
 
 std::string Template::execute()
 {
+    //NOTE: The path should probably be able to be changed
+    //      Possibly via config file
     FileLoader fileLoader("templates/"+name);
     return fileLoader.read();
 }
